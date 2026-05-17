1c0ce623ff6880e8e0714556b1a2ba87d1f97289
JimJimJC <croesjurmen@gmail.com>
2026-05-17 21:53:32 +0200

    First batch of excercise

---
+++ Excercise1.py
@@ -0,0 +1,76 @@
+x = 5
+y = 3.14
+z = "Hello"
+print(type(x))
+print(type(y))
+print(type(z)) 
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))  
+
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))  
+print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y)) 
+print(type(z))
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))  
+print(type(z))
+x = 1
+y = 2.8
+z = 1j
+a = float(x)
+b = int(y)
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+import random
+print(random.randrange(1, 10))
+x = 5
+a = int(x)
+print(type(a))
+y = 3.14
+b = float(y)
+print(type(b))
+z = 2+3j
+c = complex(z)
+print(type(c))
+x = int(1)
+y = int(2.8)
+z = int("3")
+x = float(1)
+y = float(2.8)
+z = float("3")
+z = float("4.2")
+x = str("s1")
+y = str(2)
+z = str(3.0)
+x = int(1)
+a = float(x)
+x = str("b")
+print(a, b)
